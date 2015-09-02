cd8392937bf13b98a80f170a2a8184109999a7e8
caniggiagoal <xwlpt@126.com>
2015-09-02 22:04:20 +0800

    Change the MyBookingActivity to MainActivity

--- andriod/OnTheWay/src/com/yuhj/ontheway/activity/LoginWelcomeAvtivity.java
+++ andriod/OnTheWay/src/com/yuhj/ontheway/activity/LoginWelcomeAvtivity.java
@@ -61,8 +61,10 @@ public class LoginWelcomeAvtivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				Intent intent_my_booking = new Intent(LoginWelcomeAvtivity.this, MyBookingActivity.class);
-	             startActivity(intent_my_booking);
+				 //Intent intent_my_booking = new Intent(LoginWelcomeAvtivity.this, MyBookingActivity.class);
+				
+				Intent intent_my_booking = new Intent(LoginWelcomeAvtivity.this, MainActivity.class);				 
+	            startActivity(intent_my_booking);
 			}
 		});
 		
