e59e89f80c8476959756c89bb42a53540b58819c
caniggiagoal <xwlpt@126.com>
2015-09-30 13:33:44 +0800

    1.Change the mainactivity tab icons follow pula's request 2.Add mall tab and remove booking tab

---
+++ andriod/pulastar/src/com/pula/star/fragment/MallFragment.java
@@ -0,0 +1,41 @@
+package com.pula.star.fragment;
+
+
+import org.joda.time.DateTime;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.pula.star.R;
+import com.pula.star.activity.LoginWelcomeAvtivity;
+import com.pula.star.bean.UserInfoData;
+import com.pula.star.clients.ClientApi;
+
+public class MallFragment extends Fragment {
+ 
+	
+	@Override
+	public void onCreate (Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
+ 
