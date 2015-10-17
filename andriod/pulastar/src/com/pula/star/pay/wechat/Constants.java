97f7b66f3bf815566f40d7498d6eec2bef24a919
caniggiagoal <xwlpt@126.com>
2015-10-17 21:14:37 +0800

    Add weixinpay

---
+++ andriod/pulastar/src/com/pula/star/pay/wechat/Constants.java
@@ -0,0 +1,22 @@
+package com.pula.star.pay.wechat;
+
+public class Constants {
+
+
+    //appid
+    //请同时修改  androidmanifest.xml里面，.PayActivityd里的属性<data android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
+  public static final String APP_ID = "wx1d52b098140e65b1";
+
+
+
+
+  //商户号
+   public static final String MCH_ID ="1247569801";
+
+
+//  API密钥，在商户平台设置
+    public static final  String API_KEY="uGY5LpJWfWxkjaNEpPakRRyQnzzgGyUv";
+
+
+
+}
